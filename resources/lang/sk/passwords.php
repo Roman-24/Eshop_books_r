215c8878f958626bc6af5450faf5758c79c8aa83
TenTibor <tibqor@gmail.com>
2021-11-30 14:08:00 +0100

    Preložené login formuláre

---
+++ resources/lang/sk/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bolo obnovené',
+    'sent' => 'Poslali sme vám obnovovací link na váš email',
+    'throttled' => 'Prosíme počkajte.',
+    'token' => 'Kód pre obnovovacie heslo je nesprávne.',
+    'user' => "Účet s týmto emailom neexistuje.",
+
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label class="form-label" for="password" :value="__('Password')"/>
+                <x-label class="form-label" for="password" :value="__('Heslo')"/>
                 <x-input id="password"
                          class="block mt-1 w-full"
                          type="password"
@@ -36,12 +36,12 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        Zabutnuté heslo?
                     </a>
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    Prihlásiť sa
                 </x-button>
             </div>
         </form>
