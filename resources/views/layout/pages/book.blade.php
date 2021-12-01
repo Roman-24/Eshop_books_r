6de822a9c4e6f1ecfc7d4547add39d5ace6d48a9
TenTibor <tibqor@gmail.com>
2021-12-01 09:02:45 +0100

    Book preview for admin

--- resources/views/layout/pages/book.blade.php
+++ resources/views/layout/pages/book.blade.php
@@ -7,21 +7,21 @@
         <div class="row m-0">
             <div class="image col-12 col-md-4" style="background-image: url({{asset('assets/products/'.$book->img_path)}})"></div>
             <div class="item-details col-12 col-md-8">
-                <cite class="book-title">{{$book->tittle}}</cite>
-                <div class="book-info">{{$book->author}},
-                    <time datetime="{{$book->publish_date}}" title="{{$book->publish_date}}">{{$book->publish_date}}</time>
+                <div class="item-details-top">
+                    <cite class="book-title">{{$book->tittle}}</cite>
+                    <div class="book-info">{{$book->author}},
+                        <time datetime="{{$book->publish_date}}" title="{{$book->publish_date}}">{{$book->publish_date}}</time>
+                    </div>
+                    <p class="book-description">
+                        {{$book->description}}
+                    </p>
                 </div>
-                <p class="book-description">
-                    {{$book->description}}
-                </p>
                 <div class="book-price">{{$book->price}}€</div>
                 @auth
                     @if(Auth::user()->hasRole("ADMIN"))
-{{--                @can('update', $book)--}}
-                    <a class="btn btn-warning" href="{{ URL::to('book/' . $book->id . '/edit') }}">
-                        Editovať
-                    </a>&nbsp;&nbsp;
-{{--                @endcan--}}
+                        <a class="btn btn-secondary" href="{{ URL::to('book/' . $book->id . '/edit') }}">
+                            Upraviť knihu
+                        </a>
                     @endif
                 @endauth
                 <form action="{{ route('cart.store') }}" method="POST" enctype="multipart/form-data">
@@ -29,7 +29,7 @@
                     <input type="hidden" value="{{ $book->id }}" name="id">
                     <input type="hidden" value="{{ $book->tittle }}" name="name">
                     <input type="hidden" value="{{ $book->price }}" name="price">
-                    <input type="hidden" value="{{ $book->img_path }}"  name="image">
+                    <input type="hidden" value="{{ $book->img_path }}" name="image">
                     <input type="hidden" value="1" name="quantity">
                     <button class="btn btn-primary">Pridať do košíka</button>
                 </form>
