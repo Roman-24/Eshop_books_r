80ae5422735e15aa421e8069a22d8dea22db4185
TenTibor <tibqor@gmail.com>
2021-12-05 22:42:36 +0100

    Book item into articles

--- resources/views/layout/partials/book-item.blade.php
+++ resources/views/layout/partials/book-item.blade.php
@@ -1,17 +1,19 @@
-<a href="/book/{{$book->id}}" class="book-item">
-    <div class="row m-0">
-        <div class="image col-2"
-             style="background-image: url({{asset(strlen($book->img_path)>0?'storage/products/'.$book->img_path:"assets/placeholder.jpg")}})"></div>
-        <div class="item-details col-10">
-            <cite class="book-title">{{$book->tittle}}</cite>
-            <div class="book-info">{{$book->author}},
-                <time datetime="{{$book->publish_date}}"
-                      title="{{$book->publish_date}}">{{date('d.m.Y', strtotime($book->publish_date))}}</time>
+<article class="book-item">
+    <a href="/book/{{$book->id}}">
+        <div class="row m-0">
+            <div class="image col-2"
+                 style="background-image: url({{asset(strlen($book->img_path)>0?'storage/products/'.$book->img_path:"assets/placeholder.jpg")}})"></div>
+            <div class="item-details col-10">
+                <cite class="book-title">{{$book->tittle}}</cite>
+                <div class="book-info">{{$book->author}},
+                    <time datetime="{{$book->publish_date}}"
+                          title="{{$book->publish_date}}">{{date('d.m.Y', strtotime($book->publish_date))}}</time>
+                </div>
+                <p class="book-description">
+                    {{$book->description}}
+                </p>
+                <div class="book-price">{{$book->price}}€</div>
             </div>
-            <p class="book-description">
-                {{$book->description}}
-            </p>
-            <div class="book-price">{{$book->price}}€</div>
         </div>
-    </div>
-</a>
+    </a>
+</article>
